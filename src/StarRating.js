6fa18318ec6668f04e04fae0c42ffb30a6438467
koayenay <koayenay@gmail.com>
2023-07-17 10:06:13 -0700

    proptypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import PropTypes from "prop-types"
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -7,6 +8,16 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+}
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
 }
 
 function StarRating({
