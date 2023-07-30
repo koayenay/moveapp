922fba0939d263ea80554ecfebd51a9dd76c3270
koayenay <koayenay@gmail.com>
2023-07-29 23:14:48 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -73,12 +73,15 @@ export default function App() {
   }
   useEffect(
     function () {
+      const controller = new AbortController()
+
       async function fetchMovies() {
         try {
           setIsLoading(true)
           setError("")
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           )
 
           if (!res.ok)
@@ -88,9 +91,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found!!")
 
           setMovies(data.Search)
+          setError("")
           console.log(data.Search)
         } catch (err) {
-          setError(err.message)
+          if (err.name !== "AbortError") {
+            setError(err.message)
+          }
         } finally {
           setIsLoading(false)
         }
@@ -102,6 +108,9 @@ export default function App() {
         return
       }
       fetchMovies()
+      return function () {
+        controller.abort()
+      }
     },
     [query]
   )
