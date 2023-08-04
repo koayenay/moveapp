613cdb46ec48b576b85c48f02ad6d390c1e172ae
koayenay <koayenay@gmail.com>
2023-08-04 09:24:05 -0700

    useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import StarRating from "./StarRating"
 const tempMovieData = [
   {
@@ -346,10 +346,26 @@ function NumResults({ movies }) {
 }
 
 function Search({ query, setQuery }) {
-  useEffect(function () {
-    const el = document.querySelector(".search")
-    el.focus()
-  }, [])
+  const inputEl = useRef(null)
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return
+        if (e.code === "Enter") {
+          inputEl.current.focus()
+          setQuery("")
+        }
+      }
+      document.addEventListener("keydown", callback)
+      return () => document.addEventListener("keydown", callback)
+    },
+    [setQuery]
+  )
+  // useEffect(function () {
+  //   const el = document.querySelector(".search")
+  //   el.focus()
+  // }, [])
   return (
     <input
       className='search'
@@ -357,6 +373,7 @@ function Search({ query, setQuery }) {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   )
 }
