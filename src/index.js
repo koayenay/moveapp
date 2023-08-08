3f63d4c261f5d7002e71cfaffe0464fb0f1cd09e
koayenay <koayenay@gmail.com>
2023-08-08 08:27:22 -0700

    useGeoLocation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import App from "./App"
+import Appp from "./Appp"
 import StarRating from "./StarRating"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <Appp />
     {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing!"]}
